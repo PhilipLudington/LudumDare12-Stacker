2cc197dae7f63f1c26b0b2be5b0bd34f745df1fa
Philip Ludington <mrphil@mrphilgames.com>
2012-10-06 14:54:47 -0400

    Stacker 1.2

---
+++ Transforms/Page.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Transforms
+{
+    public partial class Page : UserControl
+    {
+        static double y = 0;
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Canvas.SetTop(Blue, ++y);
+            Canvas.SetTop(Red, Canvas.GetTop(Red) + 1);
+
+            TextBlockY.Text = Convert.ToString(y);
+        }
+    }
+}
--- Transforms/Properties/AssemblyInfo.cs
+++ Transforms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Stacker")]
-[assembly: AssemblyDescription("Ludum Dare #12 entry")]
+[assembly: AssemblyTitle("Transforms")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mr. Phil Games")]
-[assembly: AssemblyProduct("Stacker 1.2")]
+[assembly: AssemblyProduct("Transforms")]
 [assembly: AssemblyCopyright("Copyright © Mr. Phil Games 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1df586fa-e1af-4d6b-a90a-cf22ce09bb8f")]
+[assembly: Guid("9ced3a14-e624-4638-8246-70697ce4cb5c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
